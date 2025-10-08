2b845d50530d0bea6676fa7a13fc1f9ab92e7b1f
Kenta Kodashima <92615334+kenta-makersights@users.noreply.github.com>
2025-10-08 16:39:08 -0700

    (#4) kk / ME-13474 Ugrade nestjs from v9 to v11
    * Update the package version
    
    * Update TS related packages
    
    * Run eslintrc migration tool
    
    * Remove old eslint config
    
    * Upgrade NestJS from V9 to V10
    
    * Upgrade NestJS from V10 to V11
    
    * Fix TS error
    
    * Add the guard to avoid init() error in the API

--- lib/event-subscribers.loader.ts
+++ lib/event-subscribers.loader.ts
@@ -3,11 +3,11 @@ import {
   OnApplicationBootstrap,
   OnApplicationShutdown,
 } from '@nestjs/common';
-import { DiscoveryService, MetadataScanner, ModuleRef} from '@nestjs/core';
+import { DiscoveryService, MetadataScanner, ModuleRef } from '@nestjs/core';
 import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
 import { EventEmitter2 } from 'eventemitter2';
 import { EventsMetadataAccessor } from './events-metadata.accessor';
-import { ScopedEvent } from "./scopedEventEmitter";
+import { ScopedEvent } from './scopedEventEmitter';
 
 function unwrapEvent(event: ScopedEvent | any): any {
   return event instanceof ScopedEvent ? event.event : event;
@@ -37,80 +37,75 @@ export class EventSubscribersLoader
     const controllers = this.discoveryService.getControllers();
 
     // Iterate over wrapped declarations of providers and controllers.
-    [...providers, ...controllers]
-      .forEach((wrapper: InstanceWrapper) => {
-          // Check if dependency tree is static for instance (All injectables are DEFAULT or TRANSIENT).
-          // And instance has already been created for static tree.
-          // Treat this as singleton.
-          if (wrapper.isDependencyTreeStatic() && wrapper.instance) {
-            this.subscribeSingleton(wrapper);
-          } else {
-            // Otherwise we do more complex handler subscription
-            this.subscribeScoped(wrapper);
-          }
-      });
-
+    [...providers, ...controllers].forEach((wrapper: InstanceWrapper) => {
+      // Check if dependency tree is static for instance (All injectables are DEFAULT or TRANSIENT).
+      // And instance has already been created for static tree.
+      // Treat this as singleton.
+      if (wrapper.isDependencyTreeStatic() && wrapper.instance) {
+        this.subscribeSingleton(wrapper);
+      } else {
+        // Otherwise we do more complex handler subscription
+        this.subscribeScoped(wrapper);
+      }
+    });
   }
 
   private subscribeScoped(wrapper: InstanceWrapper) {
     // There is no instance created, so let's create one for method analysis.
-    const prototype = wrapper.metatype?.prototype;
-    if(!prototype)
-      return;
+    const metatype = wrapper.metatype;
+    if (!metatype) return;
+    const prototype = metatype.prototype;
+    if (!prototype) return;
     const instanceForAnalysis = Object.create(prototype);
-    this.subscribeAllListeners(instanceForAnalysis,
+    this.subscribeAllListeners(
+      instanceForAnalysis,
       prototype,
-    async (args: any[]): Promise<any> => {
+      async (args: any[]): Promise<any> => {
         // We should have an event to unwrap.
-        if(args.length === 0)
-          return;
+        if (args.length === 0) return;
         // Grab context form first event.
         const event: any = args[0];
         // We don't handle non-scoped events, because they were not fired in scope and we don't know the context to handle them.
-        if (!(event instanceof ScopedEvent))
-          return;
+        if (!(event instanceof ScopedEvent)) return;
         // To resolve a class instance from NestJS container with context we need to get the module.
         // Wrapper has reference to module.
-        const moduleRef = wrapper.host!.getProviderByKey<ModuleRef>(ModuleRef).instance;
+        const moduleRef =
+          wrapper.host!.getProviderByKey<ModuleRef>(ModuleRef).instance;
         // Create or get the instance from container within the context
-        return moduleRef.resolve(wrapper.metatype, event.context);
-      }
+        return moduleRef.resolve(metatype, event.context);
+      },
     );
   }
 
   private subscribeSingleton(wrapper: InstanceWrapper) {
     const { instance } = wrapper;
     const prototype = Object.getPrototypeOf(instance) || {};
-    this.subscribeAllListeners(instance, prototype, () => Promise.resolve(instance));
+    this.subscribeAllListeners(instance, prototype, () =>
+      Promise.resolve(instance),
+    );
   }
 
   private subscribeAllListeners(
     instance: Record<string, any>,
     prototype: any,
-    getInstanceFunc: (args: any[]) => Promise<any>
+    getInstanceFunc: (args: any[]) => Promise<any>,
   ): void {
-    if(!(prototype && instance))
-      return;
-    this.metadataScanner.scanFromPrototype(
-        instance,
-        prototype,
-        methodKey => {
+    if (!(prototype && instance)) return;
+    this.metadataScanner.scanFromPrototype(instance, prototype, methodKey => {
       const method = instance[methodKey];
       // If method is private or not accessible skip it
-      if (!method)
-        return;
+      if (!method) return;
       const metadata = this.metadataAccessor.getEventHandlerMetadata(method);
-      if (!metadata)
-        return;
+      if (!metadata) return;
       // If there is no metadata for handling events - skip method
       const { event, options } = metadata;
       const listenerMethod = !!options?.prependListener
-                  ? this.eventEmitter.prependListener.bind(this.eventEmitter)
-                  : this.eventEmitter.on.bind(this.eventEmitter);
+        ? this.eventEmitter.prependListener.bind(this.eventEmitter)
+        : this.eventEmitter.on.bind(this.eventEmitter);
       const handler = async (...args: any[]) => {
         const inst = await getInstanceFunc(args);
         // In case no handling instance is resolved - skip handling
-        if(!inst) return;
+        if (!inst) return;
         return method.call(inst, ...args.map(unwrapEvent));
       };
       // Subscribe to event with wrapped handler
